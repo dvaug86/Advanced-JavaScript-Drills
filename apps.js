eccafe5fbc564196725424668db0c6bc7bb8196f
David Vaughan <dvaug86@gmail.com>
2022-03-28 11:21:13 -0400

    part 1 completed

---
+++ apps.js
@@ -0,0 +1,35 @@
+/*
+console.log ('steps 1-4')
+name = 'David';
+var name;
+console.log(name);
+
+//David with the console.log
+
+console.log('steps 5 and 6')
+name = 'David';
+let name;
+console.log(name);
+
+//error because undefined with the console.log
+
+
+console.log ('steps 7-9')
+
+setName();
+function setName(){
+    var name = 'David';
+    console.log(name);
+}
+//David is shown with console log
+
+console.log('steps10-11')
+console.log('1 text', );
+let avg = findAvg(2,2);
+console.log('2 text', avg);
+function findAvg(a,b){
+    console.log('3 text');
+    var answer = (a+b)/2;
+    return answer;
+}
+*/
